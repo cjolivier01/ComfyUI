1585f87f16eb00effdf94d25ec4cb40b1fcd0618
Chris Olivier <cjolivier01@apache.org>
2026-09-12 09:00:56 -0700

    ..

--- concatvids.sh
+++ concatvids.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
 rm -f allfiles.txt
 for i in $(find -maxdepth 1 -name "*.mp4" | shuf); do echo "file $i" >> allfiles.txt; done
+rm -f all.mp4
+ffmpeg -f concat -safe 0 -i allfiles.txt -c copy all.mp4
